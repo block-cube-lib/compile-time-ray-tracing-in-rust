66451d0e6ea74da4fabbcab43f2cd6b6d7190802
block <block.cube.lib@gmail.com>
2022-01-28 12:35:29 +0900

    feat: ピクセルの色のバッファをタプルからColorに置き換え

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
 #![feature(const_fn_floating_point_arithmetic)] // const fnの中で浮動小数点演算を行うために必要
 
 use image::ImageFormat;
+use ray_tracing_in_one_weekend_compile_time::Color;
 
 const IMAGE_WIDTH: usize = 256;
 const IMAGE_HEIGHT: usize = 256;
 const PIXEL_COUNT: usize = IMAGE_WIDTH * IMAGE_HEIGHT;
 
 fn main() -> anyhow::Result<()> {
-    const PIXEL_COLORS: [(u8, u8, u8); PIXEL_COUNT] = ray_trace();
+    const PIXEL_COLORS: [Color; PIXEL_COUNT] = ray_trace();
     const BUFFER: [u8; PIXEL_COUNT * 3] = pixels_to_buffer(PIXEL_COLORS);
     let img = image::RgbImage::from_raw(IMAGE_WIDTH as u32, IMAGE_HEIGHT as u32, BUFFER.to_vec())
         .unwrap();
@@ -16,22 +17,18 @@ fn main() -> anyhow::Result<()> {
     Ok(())
 }
 
-const fn ray_trace() -> [(u8, u8, u8); PIXEL_COUNT] {
-    let mut pixel_colors = [(0, 0, 0); PIXEL_COUNT];
+const fn ray_trace() -> [Color; PIXEL_COUNT] {
+    let mut pixel_colors = [Color::ZERO; PIXEL_COUNT];
     let mut i = 0;
     let mut j = (IMAGE_HEIGHT - 1) as i32;
     // forが使えないのでwhileで代用
     while 0 <= j {
         while i < IMAGE_WIDTH {
-            let r = (i as f64) / ((IMAGE_WIDTH - 1) as f64);
-            let g = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let b = 0.25;
-            let (r, g, b) = (
-                (r * 255.999) as u8,
-                (g * 255.999) as u8,
-                (b * 255.999) as u8,
+            pixel_colors[j as usize * IMAGE_WIDTH + i] = Color::new(
+                (i as f64) / ((IMAGE_WIDTH - 1) as f64),
+                j as f64 / (IMAGE_HEIGHT - 1) as f64,
+                0.25,
             );
-            pixel_colors[j as usize * IMAGE_WIDTH + i] = (r, g, b);
             i += 1;
         }
         i = 0;
@@ -40,10 +37,11 @@ const fn ray_trace() -> [(u8, u8, u8); PIXEL_COUNT] {
     pixel_colors
 }
 
-const fn pixels_to_buffer(pixels: [(u8, u8, u8); PIXEL_COUNT]) -> [u8; PIXEL_COUNT * 3] {
+const fn pixels_to_buffer(pixels: [Color; PIXEL_COUNT]) -> [u8; PIXEL_COUNT * 3] {
     //let buf: Vec<_> = PIXEL_COLORS
     //    .iter()
-    //    .map(|&(r, g, b)| [r, g, b])
+    //    .map(|&(r, g, b)| (r * 255.999, g * 255.999, b * 255.999))
+    //    .map(|&(r, g, b)| [r as u8, g as u8, b as u8])
     //    .flatten()
     //    .collect();
     //buf
@@ -52,9 +50,9 @@ const fn pixels_to_buffer(pixels: [(u8, u8, u8); PIXEL_COUNT]) -> [u8; PIXEL_COU
     let mut buf = [0; PIXEL_COUNT * 3];
     let mut i = 0;
     while i < PIXEL_COUNT {
-        buf[i * 3 + 0] = pixels[i].0;
-        buf[i * 3 + 1] = pixels[i].1;
-        buf[i * 3 + 2] = pixels[i].2;
+        buf[i * 3 + 0] = (pixels[i].x * 255.999) as u8;
+        buf[i * 3 + 1] = (pixels[i].y * 255.999) as u8;
+        buf[i * 3 + 2] = (pixels[i].z * 255.999) as u8;
         i += 1;
     }
     buf
