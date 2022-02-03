a93ca92594fa0d69243381648ff72c1ab674b685
block <block.cube.lib@gmail.com>
2022-02-03 16:13:56 +0900

    style: フォーマット実行

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,12 @@
 #![feature(const_mut_refs)]
 #![feature(const_eval_limit)]
 #![const_eval_limit = "0"]
+pub mod ray;
 pub mod util;
 pub mod vec3;
-pub mod ray;
 
-pub use vec3::*;
 pub use ray::*;
+pub use vec3::*;
 
 pub type Point3 = Vec3;
 pub type Color = Vec3;
